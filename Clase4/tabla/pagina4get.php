0ead63a5cb1eb76854e7c76b0ac826ffa4464201
Carlos <138796031+caglitos@users.noreply.github.com>
2025-10-11 15:55:47 +0000

    Clase 4 Tabla fix

--- Clase4/tabla/pagina4get.php
+++ Clase4/tabla/pagina4get.php
@@ -1,20 +1,26 @@
 <?php
 
-$inicio = $_GET['inicio'];
-$fin = $_GET['fin'];
-$base = $_GET['base'];
+$inicio = isset($_GET['inicio']) ? intval($_GET['inicio']) : 0;
+$fin = isset($_GET['fin']) ? intval($_GET['fin']) : 0;
+$base = isset($_GET['base']) ? intval($_GET['base']) : 1;
 
-$arreglo = array();
-
-for ($i = $inicio; $i < $fin; $i++) { 
-    $arreglo[$i] = $base * $arreglo[$i];
+if ($inicio > $fin) {
+    $tmp = $inicio;
+    $inicio = $fin;
+    $fin = $tmp;
 }
 
-echo "<h2>Tabla de multiplicar del {$numero1}</h2>";
+$arreglo = [];
+
+for ($i = $inicio; $i < $fin; $i++) {
+    $arreglo[$i] = $base * $i;
+}
+
+echo "<h2>Tabla de multiplicar del {$base}</h2>";
 echo "<table border='1'>";
 echo "<tr><th>Multiplicando</th><th>Resultado</th></tr>";
 
-for ($i = $inicio; $i < $fin; $i++) { 
+for ($i = $inicio; $i < $fin; $i++) {
     echo "<tr><td>{$base} x {$i}</td><td>" . $arreglo[$i] . "</td></tr>";
 }
 echo "</table>";
